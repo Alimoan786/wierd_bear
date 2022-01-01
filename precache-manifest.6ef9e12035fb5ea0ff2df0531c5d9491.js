b1de004f5f131a33652f59592a6a96a35f8be8eb
Alimoan786 <peoplechooser@gmail.com>
2022-01-01 21:51:23 +0530

    /

---
+++ precache-manifest.6ef9e12035fb5ea0ff2df0531c5d9491.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "db1cd8e1bb28805c469c",
+    "url": "/static/css/main.f3eaa4c3.chunk.css"
+  },
+  {
+    "revision": "db1cd8e1bb28805c469c",
+    "url": "/static/js/main.db1cd8e1.chunk.js"
+  },
+  {
+    "revision": "53333544595f973e117b",
+    "url": "/static/js/1.53333544.chunk.js"
+  },
+  {
+    "revision": "e96f1b6809c804af49cd",
+    "url": "/static/js/2.e96f1b68.chunk.js"
+  },
+  {
+    "revision": "2c681a561dfb1d0cba63",
+    "url": "/static/js/runtime~main.2c681a56.js"
+  },
+  {
+    "revision": "e63562614cbdc491e4d374f97c513b82",
+    "url": "/static/media/font.e6356261.woff"
+  },
+  {
+    "revision": "c6dd1e738dd0dafb297b0cb1ef53b6a0",
+    "url": "/index.html"
+  }
+];
